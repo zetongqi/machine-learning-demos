17b968ecbbcb882ed6ed6cce242f1a921f2d60b5
Zetong Qi <zqi38@wisc.edu>
2020-04-13 19:10:09 -0500

    Add files via upload

--- PCA.py
+++ PCA.py
@@ -1,10 +1,15 @@
 from numpy import linalg as LA
 import numpy as np
 
-def pca(X):
+def pca(X, d):
     X_bar = np.mean(X, axis=0)
     X_tilde = X-X_bar
-    S = np.matmul(np.transpose(X_tilde), X_tilde) / X_tilde.shape[0]
+    S = np.dot(X_tilde.T, X_tilde) / X_tilde.shape[0]
     v, u = LA.eig(S)
-    u_tilde = u[:,0:3]
-    return np.matmul(X, u_tilde)
+    # sort the eigenvectors according their corresponding eigenvalue
+    idx = v.argsort()[::-1]   
+    v = v[idx]
+    u = u[:,idx]
+    # project
+    u_tilde = u[:,0:d]
+    return np.dot(X, u_tilde)
